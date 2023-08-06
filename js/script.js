52a71d0a936ed3bb95e5834cdcdb3cc8bfd50c81
Jeremy Talanay <jeremytalanay@gmail.com>
2023-08-06 22:32:31 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ $('.banner-slides').slick({
   autoplay: true,
   autoplaySpeed: 12000,
 });
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
